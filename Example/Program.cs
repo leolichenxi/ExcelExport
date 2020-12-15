b7d39385085686230dc6c97fca9fd8c14bb843ea
lichenxi02 <lichenxi02@bilibili.com>
2020-12-15 20:59:01 +0800

    上传测试工程

---
+++ Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Config;
+using System.IO;
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            TestGlobalTemplate template = TestGlobalTemplate.Parser.ParseFrom(GetByte(typeof(TestGlobalTemplate)));
+            Console.WriteLine(template);
+            Console.WriteLine(template.TestObj.C);
+
+            TestGlobalTemplate template2 = TestGlobalTemplate.Parser.ParseJson(GetJson(typeof(TestGlobalTemplate)));
+            Console.WriteLine(template2);
+        }
+
+        private static string GetJson(Type t)
+        {
+            return File.ReadAllText("json/" + t.Name + ".json");
+        }
+
+        private static byte[] GetByte(Type t)
+        {
+            return File.ReadAllBytes("protobuf/"+t.Name + ".byte");
+        }
+    }
+}
